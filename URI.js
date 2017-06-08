ffb7f46a3a1c4d334c89062f1c51db605657aa09
yujin seo <mildtuna222@gmail.com>
2017-06-08 01:40:34 +0000

    uri

---
+++ URI.js
@@ -0,0 +1,29 @@
+(function(){
+
+	var input = [];
+
+	var readline = require('readline');
+	var rl = readline.createInterface({
+		input : process.stdin,
+		output : process.stdout
+	});
+
+	rl.on('line', function(line){
+		input.push(line.trim());
+	});
+
+	rl.on('close', function(){
+		
+		var cnt = input.shift();
+		
+		var enc_table =[['%20'," "],['%21','!'],['%24','$'],['%28','('],['%29',')'],['%2a','*'],['%25', '%']]; 
+		var decode = function(acc,v){
+			var patt = new RegExp(v[0],'g');
+			return acc.replace(patt,v[1]);
+		};
+
+		for(var i in input){
+			console.log(enc_table.reduce(decode, input[i]));
+		}
+	});
+})();
