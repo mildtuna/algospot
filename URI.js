08981e5ffbc3f82c1bb286e847437285257b054c
yujin seo <mildtuna222@gmail.com>
2017-08-11 12:28:03 +0000

    uri.js modified

--- URI.js
+++ URI.js
@@ -15,7 +15,6 @@
 	rl.on('close', function(){
 		
 		var cnt = input.shift();
-		var aa = 'test text';		
 		var enc_table =[['%20'," "],['%21','!'],['%24','$'],['%28','('],['%29',')'],['%2a','*'],['%25', '%']]; 
 		var decode = function(acc,v){
 			var patt = new RegExp(v[0],'g');
